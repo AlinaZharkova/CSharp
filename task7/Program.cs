1d34aed7d076ec99b39a56639860251777302cf2
AlinaZharkova <alinka--91@mail.ru>
2023-01-16 13:16:44 +0300

    123 -> 3, 789-> 9

---
+++ task7/Program.cs
@@ -0,0 +1,12 @@
+﻿//Написать программу, которая принимает на вход трехзначное число и на выходе показывает последнюю цифру этого числа.
+System.Console.WriteLine("Введите трехзначное число:");
+int a = Convert.ToInt32(Console.ReadLine());
+if ((a < 100) || (a > 999))
+{
+    System.Console.WriteLine("ошибка!");
+}
+else
+{
+    int b = a % 10;
+System.Console.WriteLine(b);
+}
