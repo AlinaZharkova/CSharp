480d651f999a86fbab88ea8014d6db740bbbbbfc
AlinaZharkova <alinka--91@mail.ru>
2023-01-16 14:30:38 +0300

    N -> [1,N]%2 == 0

---
+++ task8/Program.cs
@@ -0,0 +1,13 @@
+﻿//Написать программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+System.Console.WriteLine("Введите число:");
+
+int a = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+
+while (i < a)
+{
+    i += 1;
+    System.Console.Write(i + " ");
+    i++;
+}
+
