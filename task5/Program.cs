c874ed1cad927eb6f7e5d1de48190c4001f57b81
AlinaZharkova <alinka--91@mail.ru>
2023-01-16 12:41:38 +0300

    N -> [-N,N]

---
+++ task5/Program.cs
@@ -0,0 +1,12 @@
+﻿//Написать программу, которая на вход принимает число N, а на выходе выдает все целые числа от -N до N.
+System.Console.WriteLine("Введите число:");
+
+int a = Convert.ToInt32(Console.ReadLine());
+
+int i = -1 * a;
+
+while (i <= a)
+{
+    System.Console.Write(i + " ");
+    i++;
+}
