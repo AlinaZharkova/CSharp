36dd17921f96fecfcaf2e810ed907867ba66c5ee
AlinaZharkova <alinka--91@mail.ru>
2023-01-18 13:52:05 +0300

    8 -> четное число, 5-> нечетное число

---
+++ task6/Program.cs
@@ -0,0 +1,12 @@
+﻿//Написать программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+System.Console.WriteLine("Введите число:");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a%2==0)
+{
+  System.Console.Write("четное число");
+}
+else
+{
+  System.Console.Write("нечетное число");
+}
