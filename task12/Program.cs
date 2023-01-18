fc5c4d2dcd942f44470ac879bfa576dbcbac6e64
AlinaZharkova <alinka--91@mail.ru>
2023-01-19 00:39:38 +0300

    34, 5 -> не кратно, остаток 4.   16, 4 -> кратно

---
+++ task12/Program.cs
@@ -0,0 +1,21 @@
+﻿// Написать программу, которая принимает на вход 2 числа и проверяет является ли первое число кратным второму.
+// Если число 1 не кратно 2, то вывести остаток от деления (34, 5 -> не кратно, остаток 4.   16, 4 -> кратно)
+int InputNumber() // вводим функцию для ввода целого числа с клавиатуры
+{
+    Console.Write("Введите число: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int firstNum = InputNumber();
+int secondNum = InputNumber();
+
+if (firstNum % secondNum == 0)
+{
+    System.Console.WriteLine("кратно");
+}
+
+else
+{
+    System.Console.WriteLine("не кратно, остаток " + firstNum % secondNum);
+}
