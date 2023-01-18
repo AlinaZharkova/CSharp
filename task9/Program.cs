2f2b6314c02897678d9d6e91e1d1f318c70c5df4
AlinaZharkova <alinka--91@mail.ru>
2023-01-18 20:23:41 +0300

    59-> max=9

---
+++ task9/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка [10,99] 
+//и показывает наибольшую цифру числа (85->8, 14->4).
+
+int Max(int a, int b) // вводим функцию для сравнения 2 чисел между собой
+{
+    if (a>b) return a;
+    else return b;
+}
+
+int a = new Random().Next(10, 100);
+System.Console.WriteLine($"Random = {a}");
+
+int firstNum = a / 10; //целочисленное деление на 10
+int secondNum = a % 10; //остаток от целочисленного деления на 10
+
+int max= Max(firstNum, secondNum);
+
+System.Console.WriteLine($"Max = {max}");
+
+
+
