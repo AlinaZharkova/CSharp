6594e3e5d398daa1d98e516e1b36c561f73a39ab
AlinaZharkova <alinka--91@mail.ru>
2023-01-18 23:40:57 +0300

    Random 567-> 57

---
+++ task11/Program.cs
@@ -0,0 +1,8 @@
+﻿// Написать программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа (456->46)
+Random num = new Random();
+int a = num.Next(100, 1000);
+System.Console.WriteLine($"Случайное трехзначное число {a}");
+int firstNum = a / 100;
+int thirdNum = a % 10;
+a = firstNum * 10 + thirdNum;
+System.Console.WriteLine($"Двузначное число {a}");
