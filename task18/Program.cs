b960506625b6e486dd193d8f8ab89b2ae4dfed62
AlinaZharkova <alinka--91@mail.ru>
2023-01-25 00:08:10 +0300

    номер четверти -> (x и y)

---
+++ task18/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая по заданному номеру четверти показывает диапазон 
+//возможных координат точек в этой четверти (x и y)
+Console.Write("Введите значение четверти координатной плоскости: ");
+int quarter = Convert.ToInt32(Console.ReadLine());
+
+switch(quarter) {
+case 1:
+    System.Console.WriteLine("x>0, y>0");
+break;
+
+case 2:
+    System.Console.WriteLine("x<0, y>0");
+break;
+
+case 3:
+    System.Console.WriteLine("x<0, y<0");
+break;
+
+case 4:
+    System.Console.WriteLine("x>0, y<0");
+break;
+
+default:
+System.Console.WriteLine("На координатной плоскости только 4 четверти! Введите число от 1 до 4");
+break; }
--- task23/Program.cs
+++ task23/Program.cs
@@ -1,10 +1,8 @@
 ﻿//Написать программу, которая на вход принимает число (N), а на выходе выдвет таблицу кубов чисел от 1 до N. (5-> 1,8,27,64,125)
 System.Console.WriteLine("Введите число:");
 int n = Convert.ToInt32(Console.ReadLine());
-double cube = 1;
-   
-    for 
-    (int i =1; i<= n; i++)
+double cube = 1; 
+    for (int i =1; i<= n; i++)
 {
 
     cube = Math.Pow(i, 3);
