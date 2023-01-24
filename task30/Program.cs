2371b12cb3e67e535ea2ce03354fbf3278438568
AlinaZharkova <alinka--91@mail.ru>
2023-01-24 23:48:56 +0300

    массив [8] -> 0 и 1 в случайном порядке

---
+++ task30/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, которая выводит массив из 8 элементов, заполненный 0 и 1 в случайном порядке.
+int[] a = new int[8];
+Random rnd = new Random();
+for (int i = 0; i < 8; i++)
+{
+    a[i]= rnd.Next(0,2);
+    System.Console.WriteLine(a[i]);
+}
