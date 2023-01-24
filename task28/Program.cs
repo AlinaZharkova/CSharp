31475c0cb04d693c9bf759c3e8d87f1dd7468357
AlinaZharkova <alinka--91@mail.ru>
2023-01-24 23:44:11 +0300

    -->N рез-т произведение от 1 до N (4->24, 5->120)

---
+++ task28/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдает произведение чисел от 1 до N (4->24, 5->120)
+System.Console.WriteLine("Введите число: ");
+
+int N = Convert.ToInt32(Console.ReadLine());
+int product=1;
+for (int i = 1; i <= N; i++)
+{
+product=product*i;    
+}
+System.Console.WriteLine("product = " +product);
