614a65a1ba18872c90f99227c12ce643de742abe
AlinaZharkova <alinka--91@mail.ru>
2023-01-25 00:48:27 +0300

    расстояние между А(3,б) и В(2,1) -> 5,09

---
+++ task20/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход координаты 2 точек и находит расстояние 
+//между ними в 2D-пространстве А(3,б); В(2,1) -> 5,09
+System.Console.WriteLine("Введите координату точки A по оси X: ");
+double coordinateAX = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите координату точки A по оси Y: ");
+double coordinateAY = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите координату точки B по оси X: ");
+double coordinateBX = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите координату точки B по оси Y: ");
+double coordinateBY = Convert.ToInt32(Console.ReadLine());
+
+double distance = Math.Sqrt(Math.Pow(coordinateBX - coordinateAX, 2) + Math.Pow(coordinateBY - coordinateAY,2));
+System.Console.WriteLine($"Расстояние между точками A и B на плоскости = " + "{0:0.00}", distance);
