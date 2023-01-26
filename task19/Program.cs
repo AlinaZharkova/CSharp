469d3c541335e3c3b058d125901696757c379123
AlinaZharkova <alinka--91@mail.ru>
2023-01-27 00:25:46 +0300

    12321 -> палиндром, 45678 -> не палиндром

---
+++ task19/Program.cs
@@ -0,0 +1,27 @@
+﻿//Напишите программу, которая на вход принимает пятизначное число и проверяет является ли 
+//оно палиндромом.
+
+System.Console.WriteLine("введите пятизначное число: ");
+
+string? number = Console.ReadLine();
+
+void CheckNumber(string number)
+{
+    if (number[0] == number[4] || number[1] == number[3])
+    {
+    Console.WriteLine($"Число {number} является палиндромом");
+    }
+
+    else 
+    {
+    Console.WriteLine($"Число {number} - не палиндром");
+    }
+}
+
+    if (number!.Length == 5) 
+    {
+        CheckNumber(number);
+    }
+    else 
+    
+        Console.WriteLine($"Введите Пятизначное число!");
