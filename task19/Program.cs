33269798ee81a6205b27a3cccb8656ea5c3ab32d
AlinaZharkova <alinka--91@mail.ru>
2023-01-27 09:53:30 +0300

    12321-> палиндром, 67890-> не палиндром

--- task19/Program.cs
+++ task19/Program.cs
@@ -7,7 +7,7 @@ string? number = Console.ReadLine();
 
 void CheckNumber(string number)
 {
-    if (number[0] == number[4] || number[1] == number[3])
+    if (number[0] == number[4] && number[1] == number[3])
     {
     Console.WriteLine($"Число {number} является палиндромом");
     }
