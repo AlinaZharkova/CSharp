6c64e1c68b658c6b270fbbf490cb6594075a979f
AlinaZharkova <alinka--91@mail.ru>
2023-01-28 23:00:35 +0300

    массив [12] из отрезка, сумма полож. и отриц чисел

---
+++ task31/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задайте массив из 12 элементов, заполненный случайными числами из промежутка [-9,9]. 
+// Найдите сумму отрицательных и положительных элементов массива.
+int[] arr=new int[12];
+int sumOtric=0;
+int sumPolozh=0;
+
+Random rnd = new Random();
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i]=rnd.Next(-9,10);
+}
+
+foreach (var item in arr)
+{
+    System.Console.Write(item + ", ");
+}
+
+foreach (var item in arr)
+{
+    if (item<0)
+    {
+        sumOtric+=item;
+    }
+    else sumPolozh+=item;
+}
+System.Console.WriteLine();
+System.Console.WriteLine($"сумма отрицательных чисел = {sumOtric}; " + $"сумма положительных чисел = {sumPolozh}.");
