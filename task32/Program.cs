d94f0a733a0a6136dd39c92e432cb6aaa2a91817
AlinaZharkova <alinka--91@mail.ru>
2023-01-28 23:08:59 +0300

    [-4,-8,8,2] -> [4,8,-8,-2]

---
+++ task32/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу замены элементов массива: положительные элементы замените на соответсвующие 
+//отрицательные и наоброт. [-4,-8,8,2] -> [4,8,-8,-2]
+
+int[] array=new int[8];
+Random rnd=new Random();
+for (int i = 0; i < array.Length; i++)
+{
+    array[i]=rnd.Next(-99,100);
+    System.Console.Write($"{array[i]}, ");
+}
+System.Console.WriteLine();
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i]=array[i]*-1;
+    System.Console.Write($"{array[i]}, ");
+}
