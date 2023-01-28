fd6268ee7494d7da053ba142ceb8603f5a6d2a55
AlinaZharkova <alinka--91@mail.ru>
2023-01-28 22:48:02 +0300

    количество эл-тов массива [123] в отрезке [10,99]

---
+++ task35/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задайте одномерный массив из 123 случайных чисел. Найдите количество элементов массива, 
+//значения которых лежат в отрезке [10,99]
+int[] array = new int[123];
+Random rnd = new Random();
+int count = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = rnd.Next(0,124);
+    System.Console.Write($"{array[i]}, ");
+    if (array[i]>9 && array[i]<100) count+=1;
+}
+System.Console.WriteLine();
+System.Console.WriteLine("count = " + count);
