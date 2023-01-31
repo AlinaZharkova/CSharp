d1adc856054c3d8750570e77bcf737e5ddfe4bf3
AlinaZharkova <alinka--91@mail.ru>
2023-02-01 01:50:59 +0300

    программа для вывода чисел Фибоначчи(без рекурсии)

---
+++ task44/Program.cs
@@ -0,0 +1,23 @@
+﻿// Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые 2 числа: 0 и 1.
+// Если n=5, то 01123, если n=7, то 0112358
+
+int InputNumber()
+{
+    Console.Write("Введите число: ");
+    int x = Convert.ToInt32(Console.ReadLine());
+    return x;
+}
+
+int n = InputNumber();
+int a = 0;
+int b = 1;
+int temp = 0;
+
+for (int i = 0; i < n; i++)
+{
+    Console.Write(a + " ");
+    temp = a;
+    a = b;
+    b = a + temp;
+}
+
