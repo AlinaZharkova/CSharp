04438a4ea22816ede297dca547356bf0d220cc59
AlinaZharkova <alinka--91@mail.ru>
2023-02-01 03:19:52 +0300

    поэлементное копирование массива в другой массив

---
+++ task45/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая будет создавать копию заданного массива с помощью поэлементного копирования.
+
+int[] array = {1,2,3,4,5,6,7};
+int[] arrayCopy = new int[array.Length];
+
+for (int i = 0; i < array.Length; i++)
+{
+    arrayCopy[i]=array[i];
+}
+
+foreach (var item in arrayCopy)
+{
+    System.Console.WriteLine(item);
+}
