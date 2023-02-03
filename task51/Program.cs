079528345afc991992c8187e57ca86e5f9949ed6
AlinaZharkova <alinka--91@mail.ru>
2023-02-03 23:46:13 +0300

    сумма эл-тов массива идущих по главной диагонали

---
+++ task51/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задайте двумерный массив. Найдите сумму элементов, находящихся на главной диагонали 
+//(с индексами (0,0), (1,1), (2,2) и т.д.) 
+
+System.Console.Write("Задайте число строк: ");
+int m = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Задайте число столбцов: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = new int[m, n]; 
+Random rnd = new Random();
+int sum=0;
+
+ for (int i = 0; i < array.GetLength(0); i++) 
+    {
+        for (int j = 0; j < array.GetLength(1); j++) 
+        {
+            array[i, j] = rnd.Next(1, 10);
+            System.Console.Write(array[i,j] + " ");
+        }
+        System.Console.WriteLine();
+    }
+
+System.Console.WriteLine();
+
+    for (int i = 0; i < array.GetLength(0); i++) 
+    {
+        for (int j = 0; j < array.GetLength(1); j++) 
+        {
+            if (i==j)  sum+= array[i,j];  
+        }
+    }
+  System.Console.WriteLine(sum);
