afa4460a1af9c2f530e27dd7a419870084cb010b
AlinaZharkova <alinka--91@mail.ru>
2023-02-08 13:47:37 +0300

    массив, заполненный случ.вещественнными числами

--- task47/Program.cs
+++ task47/Program.cs
@@ -36,7 +36,7 @@ for (int i = 0; i < arr.GetLength(0); i++)
 {
     for (int j = 0; j < arr.GetLength(1); j++)
     {
-      Console.Write(Math.Round(arr[i,j],2) + " " );
+      Console.Write(String.Format("{0,9}", Math.Round(arr[i,j],2) + " " ));
     }
     System.Console.WriteLine();
 }
