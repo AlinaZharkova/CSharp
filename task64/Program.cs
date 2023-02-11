de3521f8101a236092304ca98aae7813db6069d3
AlinaZharkova <alinka--91@mail.ru>
2023-02-11 05:28:56 +0300

    ReversNaturalNumbers 5 4 3 2 1

---
+++ task64/Program.cs
@@ -0,0 +1,24 @@
+﻿//Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке 
+//от N до 1. Выполнить с помощью рекурсии.
+//N = 5 -> "5, 4, 3, 2, 1"
+//N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+Console.WriteLine("Введите натуральное число от 1: ");
+int N = Convert.ToInt32(Console.ReadLine());
+void ReverseNumbers(int N)
+{
+    if (N <= 0) 
+    {
+        Console.WriteLine($"{N} - не натуральное число");
+        return;
+    }
+    else 
+    {
+        System.Console.Write(N + " ");
+        ReverseNumbers(N - 1);
+    }
+}
+ 
+ ReverseNumbers(N);
+ 
+
---
+++ task66/Program.cs
@@ -0,0 +1,30 @@
+﻿//Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов 
+//в промежутке от M до N.
+//M = 1; N = 15 -> 120
+//M = 4; N = 8. -> 30
+
+Console.WriteLine("Введите натуральное число M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите натуральное число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int temp = M;
+
+if (M > N) 
+{
+  M = N; 
+  N = temp;
+}
+
+PrintSum(M, N, temp=0);
+
+void PrintSum(int M, int N, int sum)
+{
+  sum+= N;
+  if (N <= M)
+  {
+    Console.Write($"Сумма элементов в промежутке от M до N = {sum} ");
+    return;
+  }
+  PrintSum(M, N - 1, sum);
+}
